0b876b1eac036b06bb7607e64c983d2cdb3853b7
fatih <faaydemir@outlook.com>
2022-12-16 22:41:02 +0300

    add sample snippet file

---
+++ sample-snippet-files/js-snippet.js
@@ -0,0 +1,78 @@
+module.exports = {
+    commands: [
+        {
+            name: "Refactor ",
+            template: `Refactor given {system.language} code.
+            code:
+            {system.selection}`,
+            handler: 'replace'
+        },
+        {
+            name: "Create unit test.",
+            template: `Create unit test in {user.unitTestFramework} framework for following function. 
+            code:
+            {system.selection}`,
+            handler: {
+                func: 'writeFile',
+                args: {
+                    filePath: 'user.testFileName'
+                }
+            }
+        },
+        {
+            name: "Complete.",
+            template: `Complete following {system.language} function. Just give code
+            code:
+            {system.selection}`,
+            handler:{
+                func: 'append',
+                args: {
+                    position: 'end'
+                }
+            }
+        },
+        {
+            name: "Explain following code",
+            template: `Explain following code. And show me what can be improvements.
+            code:
+            {system.selection}`,
+            handler: {
+                func: 'showWebView',
+            }
+        },
+        {
+            name: "Write jsdoc",
+            template: `Write jsdoc for following functions.
+            code:
+            {system.selection}`,
+            handler: {
+                func: 'append',
+                args: {
+                    position: 'user.start'
+                }
+            }
+        },
+    ],
+    functions: [
+        {
+        }
+    ],
+    variables: [
+        {
+            name: "unitTestFramework",
+            value: "test"
+        },
+        {
+            name: "start",
+            value: "start"
+        },
+        {
+            name: "end",
+            value: "end"
+        },
+        {
+            name: "testFileName",
+            value: ({ baseFolder,fileName,fileExtension }) => `${baseFolder}\\tests\\${fileName}.test.${fileExtension}`
+        },
+    ]
+};
