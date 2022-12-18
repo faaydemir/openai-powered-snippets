913e4775f4aa959dadc1331879fceacb506866bf
fatih <faaydemir@outlook.com>
2022-12-18 15:49:51 +0300

    fix multiple channel creation bug

--- src/vscode-functions/write-console.js
+++ src/vscode-functions/write-console.js
@@ -1,6 +1,12 @@
+
 export default function consoleWrite(text, channelName = "openai-powered-snip") {
+    consoleWrite.channles = consoleWrite.channles ?? {};
     let vscode = require("vscode");
-    let channel = vscode.window.createOutputChannel(channelName);
+    if (!consoleWrite.channles[channelName]) {
+        consoleWrite.channles[channelName] = vscode.window.createOutputChannel(channelName);
+    }
+    let channel = consoleWrite.channles[channelName];
+    channel.show(true);
     if (channel) {
         channel.appendLine(text);
     }
